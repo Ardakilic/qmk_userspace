03fd057264abe4b7988847ac305170bc5b2b90ba
Arda Kılıçdağı <ardakilicdagi@gmail.com>
2025-11-12 21:07:01 +0300

    Revert "chore: enrich cost_crafter40 keymap"
    This reverts commit fe0d38d579f2e556a9a80fe099ad12afcf9d3816.

--- keyboards/cost_crafter40/keymaps/ardakilic/config.h
+++ keyboards/cost_crafter40/keymaps/ardakilic/config.h
@@ -3,19 +3,3 @@
 #define I2C1_SDA_PIN GP18
 #define I2C1_SCL_PIN GP19
 #define I2C_DRIVER I2CD1
-
-// Combo configuration
-#define COMBO_TERM 50
-#define COMBO_MUST_TAP_PER_COMBO
-#define COMBO_TERM_PER_COMBO
-
-// Enable OS Detection
-#define OS_DETECTION_ENABLE
-
-// Tapping term for layer tap and mod tap keys
-#define TAPPING_TERM 200
-#define TAPPING_TERM_PER_KEY
-
-// Prevent accidental modifier activations
-#define IGNORE_MOD_TAP_INTERRUPT
-#define PERMISSIVE_HOLD
