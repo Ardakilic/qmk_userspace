e47b0b6cbc9f13f6cc73d259198adf14a7218668
Arda Kılıçdağı <ardakilicdagi@gmail.com>
2025-11-12 21:33:42 +0300

    Reapply "chore: enrich cost_crafter40 keymap"
    This reverts commit 03fd057264abe4b7988847ac305170bc5b2b90ba.

--- keyboards/cost_crafter40/keymaps/ardakilic/keymap.c
+++ keyboards/cost_crafter40/keymaps/ardakilic/keymap.c
@@ -21,7 +21,9 @@ enum cost_crafter40_layers {
   _LOWER,
   _RAISE,
   _ADJUST,
-  _NUMPAD
+  _NUMPAD,
+  _SPACE,
+  _MAC
 };
 
 // KC_NONUS_BACKSLASH (\|) is equivalent to [<>|] key in Turkish keyboards.
@@ -30,6 +32,8 @@ enum cost_crafter40_layers {
 // KC_QUOT is Turkish i [iİ] key
 // KC_COMM is Turkish o [öÖ] key
 // KC_DOT is Turkish c [çÇ] key
+// KC_LBRC is Turkish g [ğĞ] key
+// KC_RBRC is Turkish u [üÜ] key
 
 // Custom shortcuts specific to Turkish layout
 #define CURLY_OPEN RALT(KC_7)
@@ -46,39 +50,56 @@ enum cost_crafter40_layers {
 #define GREATER_THAN RALT(KC_1)
 #define LOCKSCREEN LCTL(LSFT(KC_PWR)) // Screen Lock shortcut for OSX
 
+enum custom_keycodes {
+    MOUSE_INFINITE_TOGGLE = SAFE_RANGE,
+    MO_NUMPAD
+};
+
 // Combo keys for missing keys
 enum combos {
   C_ESC,
   C_DEL,
   C_TAB,
-  C_ENTER,
+  C_BSPC,
   C_CTRL,
-  C_ALT,
-  C_RAISE,
-  C_ADJUST,
-  C_NUMPAD
+  C_GUI,
+  C_NUMPAD_LAYER,
+  C_LEFT_MOUSE_BTN_1,
+  C_LEFT_MOUSE_BTN_2,
+  C_RIGHT_MOUSE_BTN_1,
+  C_RIGHT_MOUSE_BTN_2,
+  C_MIDDLE_MOUSE_BTN_1,
+  C_MIDDLE_MOUSE_BTN_2
 };
 
-const uint16_t PROGMEM c_esc_combo[] = {KC_Q, KC_A, COMBO_END};
+const uint16_t PROGMEM c_esc_combo[] = {KC_Q, KC_W, COMBO_END};
 const uint16_t PROGMEM c_del_combo[] = {KC_O, KC_P, COMBO_END};
-const uint16_t PROGMEM c_tab_combo[] = {KC_Q, KC_W, COMBO_END};
-const uint16_t PROGMEM c_enter_combo[] = {KC_SCLN, KC_SLSH, COMBO_END};
+const uint16_t PROGMEM c_tab_combo[] = {KC_A, KC_S, COMBO_END};
+const uint16_t PROGMEM c_bspc_combo[] = {KC_L, KC_SCLN, COMBO_END};
 const uint16_t PROGMEM c_ctrl_combo[] = {KC_X, KC_C, COMBO_END};
-const uint16_t PROGMEM c_alt_combo[] = {KC_C, KC_V, COMBO_END};
-const uint16_t PROGMEM c_raise_combo[] = {KC_LGUI, KC_SPC, COMBO_END};
-const uint16_t PROGMEM c_adjust_combo[] = {KC_LGUI, KC_ENT, COMBO_END};
-const uint16_t PROGMEM c_numpad_combo[] = {KC_A, KC_Z, COMBO_END};
+const uint16_t PROGMEM c_gui_combo[] = {KC_C, KC_V, COMBO_END};
+const uint16_t PROGMEM c_numpad_layer_combo[] = {KC_Q, KC_A, COMBO_END};
+const uint16_t PROGMEM c_left_mouse_btn_1_combo[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM c_left_mouse_btn_2_combo[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM c_right_mouse_btn_1_combo[] = {KC_V, KC_B, COMBO_END};
+const uint16_t PROGMEM c_right_mouse_btn_2_combo[] = {KC_N, KC_M, COMBO_END};
+const uint16_t PROGMEM c_middle_mouse_btn_1_combo[] = {KC_F, KC_G, COMBO_END};
+const uint16_t PROGMEM c_middle_mouse_btn_2_combo[] = {KC_H, KC_J, COMBO_END};
 
 combo_t key_combos[] = {
     [C_ESC] = COMBO(c_esc_combo, KC_ESC),
     [C_DEL] = COMBO(c_del_combo, KC_DEL),
     [C_TAB] = COMBO(c_tab_combo, KC_TAB),
-    [C_ENTER] = COMBO(c_enter_combo, KC_ENT),
+    [C_BSPC] = COMBO(c_bspc_combo, KC_BSPC),
     [C_CTRL] = COMBO(c_ctrl_combo, KC_LCTL),
-    [C_ALT] = COMBO(c_alt_combo, KC_LALT),
-    [C_RAISE] = COMBO(c_raise_combo, MO(_RAISE)),
-    [C_ADJUST] = COMBO(c_adjust_combo, MO(_ADJUST)),
-    [C_NUMPAD] = COMBO(c_numpad_combo, MO(_NUMPAD)),
+    [C_GUI] = COMBO(c_gui_combo, KC_LGUI),
+    [C_NUMPAD_LAYER] = COMBO(c_numpad_layer_combo, MO_NUMPAD),
+    [C_LEFT_MOUSE_BTN_1] = COMBO(c_left_mouse_btn_1_combo, KC_MS_BTN1),
+    [C_LEFT_MOUSE_BTN_2] = COMBO(c_left_mouse_btn_2_combo, KC_MS_BTN1),
+    [C_RIGHT_MOUSE_BTN_1] = COMBO(c_right_mouse_btn_1_combo, KC_MS_BTN2),
+    [C_RIGHT_MOUSE_BTN_2] = COMBO(c_right_mouse_btn_2_combo, KC_MS_BTN2),
+    [C_MIDDLE_MOUSE_BTN_1] = COMBO(c_middle_mouse_btn_1_combo, KC_MS_BTN3),
+    [C_MIDDLE_MOUSE_BTN_2] = COMBO(c_middle_mouse_btn_2_combo, KC_MS_BTN3)
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -91,70 +112,73 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
  * │Z/⇧│ X │ C │ V │ B │ N │ M │ Ö │ Ç │./⇧│
  * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
- * │ALT│CTL│CMD│LWR│SPC│ENT│ ← │ ↓ │ ↑ │ → │
+ * │"/N│<>/│Alt│Cmd│Lwr│Spc│ ← │ ↓ │ ↑ │ → │
  * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
  *
- * Combos: Q+A=ESC, O+P=DEL, Q+W=TAB, Ş+./⇧=ENT, X+C=CTRL, C+V=ALT, CMD+SPC=RAISE, CMD+ENT=ADJUST, A+Z=NUMPAD
+ * Combos: Q+W=ESC, O+P=DEL, A+S=TAB, L+Ş=BSPC, X+C=CTRL, C+V=GUI, Q+A=NUMPAD(hold)
+ * Mouse: D+F=LMB, J+K=LMB, V+B=RMB, N+M=RMB, F+G=MMB, H+J=MMB
+ * Layer Taps: "=Numpad (hold), Space=SPACE layer (hold), <>|=Ctrl (hold)
  */
 [_QWERTY] = LAYOUT(
-    KC_Q,          KC_W,    KC_E,    KC_R,               KC_T,            KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,
-    KC_A,          KC_S,    KC_D,    KC_F,               KC_G,            KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN,
-    LSFT_T(KC_Z),  KC_X,    KC_C,    KC_V,               KC_B,            KC_N,    KC_M,     KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
-    KC_LALT,       LCTL_T(KC_GRV), KC_LGUI, MO(_LOWER), LT(_RAISE, KC_SPC), KC_ENT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_Q,               KC_W,    KC_E,    KC_R,     KC_T,            KC_Y,                   KC_U,     KC_I,    KC_O,    KC_P,
+    KC_A,               KC_S,    KC_D,    KC_F,     KC_G,            KC_H,                   KC_J,     KC_K,    KC_L,    KC_SCLN,
+    LSFT_T(KC_Z),       KC_X,    KC_C,    KC_V,     KC_B,            KC_N,                   KC_M,     KC_COMM, KC_DOT,  RSFT_T(KC_SLSH),
+    LT(_NUMPAD, KC_GRV), LCTL_T(KC_NONUS_BACKSLASH), KC_LALT, KC_LGUI, MO(_LOWER), LT(_SPACE, KC_SPC), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* LOWER (Function Keys & Symbols)
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
- * │F1 │F2 │F3 │F4 │F5 │ \ │ | │ - │ = │ _ │
+ * │F1 │F2 │F3 │F4 │F5 │F6 │ \ │ | │ - │ = │
  * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
- * │F6 │F7 │F8 │F9 │F10│ # │ ` │ İ │ Ğ │ Ü │
+ * │F7 │F8 │F9 │F10│F11│F12│ # │ ` │ İ │ Ğ │
  * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
- * │F11│F12│ * │ ? │ @ │ $ │ { │ } │ < │ > │
+ * │   │   │ * │ ? │ @ │ $ │ { │ } │ Ü │ _ │
  * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
- * │ % │ ! │ & │   │ ^ │ ~ │ ( │ ) │ [ │ ] │
+ * │   │ % │ ! │ & │   │ ^ │ ~ │ ( │ ) │ + │
  * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
  */
 [_LOWER] = LAYOUT(
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   BACKSLASH,    VERTICAL_PIPE, KC_MINS,       KC_EQL,        KC_UNDS,
-    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  HASHTAG,      BACKTICK,      KC_QUOT,       KC_LBRC,       KC_RBRC,
-    KC_F11,  KC_F12,  KC_ASTR, KC_QUES, KC_AT,   DOLLAR_SIGN,  CURLY_OPEN,    CURLY_CLOSE,   LESS_THAN,     GREATER_THAN,
-    KC_PERC, KC_EXLM, KC_AMPR, _______, KC_CIRC, TILDE,        KC_LPRN,       KC_RPRN,       SQUARE_OPEN,   SQUARE_CLOSE
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,        BACKSLASH,    VERTICAL_PIPE, KC_MINS,       KC_EQL,
+    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,       HASHTAG,      BACKTICK,      KC_QUOT,       KC_LBRC,
+    _______, _______, KC_ASTR, KC_QUES, KC_AT,   DOLLAR_SIGN,  CURLY_OPEN,   CURLY_CLOSE,   KC_RBRC,       KC_UNDS,
+    _______, KC_PERC, KC_EXLM, KC_AMPR, _______, KC_CIRC,      TILDE,        KC_LPRN,       KC_RPRN,       KC_PLUS
 ),
 
 /* RAISE (Numbers & Symbols)
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
  * │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │
  * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
- * │   │   │   │   │   │   │   │ ` │ ~ │ + │
+ * │   │   │   │   │   │   │ * │ - │ \ │ | │
  * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
- * │   │   │   │   │   │   │   │ [ │ ] │ | │
+ * │   │   │   │   │   │   │ # │ " │ ~ │ ` │
  * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
- * │   │   │   │   │   │   │   │   │   │ _ │
+ * │   │   │   │   │   │   │   │ < │ [ │ ] │
  * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
  */
 [_RAISE] = LAYOUT(
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,           KC_9,           KC_0,
-    _______, _______, _______, _______, _______, _______, _______, BACKTICK,       TILDE,          KC_PLUS,
-    _______, _______, _______, _______, _______, _______, _______, SQUARE_OPEN,    SQUARE_CLOSE,   VERTICAL_PIPE,
-    _______, _______, _______, _______, _______, _______, _______, _______,        _______,        KC_UNDS
+    _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,         BACKSLASH,      VERTICAL_PIPE,
+    _______, _______, _______, _______, _______, _______, HASHTAG, KC_GRV,         TILDE,          BACKTICK,
+    _______, _______, _______, _______, _______, _______, _______, LESS_THAN,      SQUARE_OPEN,    SQUARE_CLOSE
 ),
 
 /* ADJUST (System & Settings)
+ * Accessed via LOWER + RAISE (layer combination)
  * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
  * │RST│   │   │   │   │   │   │   │   │LCK│
  * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
- * │   │   │   │   │   │   │VL-│VL+│   │   │
+ * │CAP│   │   │   │   │   │   │   │   │   │
  * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
- * │CAP│   │   │   │   │PRV│PLY│NXT│   │   │
+ * │   │   │   │   │   │PRV│PLY│NXT│VL-│VL+│
  * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
- * │   │   │   │   │   │   │   │   │   │   │
+ * │   │   │   │MAC│   │ENT│BR-│   │ ↑ │BR+│
  * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
  */
 [_ADJUST] = LAYOUT(
     QK_BOOT, _______, _______, _______, _______, _______, _______, _______, _______, LOCKSCREEN,
-    _______, _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, _______, _______,
-    KC_CAPS, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, KC_VOLU,
+    _______, _______, _______, TG(_MAC), _______, KC_ENT, KC_BRMD, _______, KC_UP,   KC_BRMU
 ),
 
 /* NUMPAD
@@ -172,7 +196,141 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, KC_NUM,  KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PMNS,
     _______, _______, _______, _______, _______, KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
     _______, _______, _______, _______, _______, KC_PEQL, KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-    _______, _______, _______, _______, KC_BSPC, KC_PDOT, KC_P0,   KC_P0,   KC_PDOT, KC_PENT
+    _______, _______, _______, _______, KC_BSPC, KC_PDOT, KC_P0,   KC_P0,   KC_SLSH, KC_PENT
+),
+
+/* SPACE (CMD+Number shortcuts & Navigation)
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+ * │⌘1 │⌘2 │⌘3 │⌘4 │⌘5 │⌘6 │⌘7 │⌘8 │⌘9 │⌘0 │
+ * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+ * │   │   │   │   │   │   │   │   │ ↑ │   │
+ * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+ * │   │   │   │   │   │   │   │ ← │ ↓ │ → │
+ * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+ * │   │   │   │   │   │   │   │BR-│ ↑ │BR+│
+ * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+ */
+[_SPACE] = LAYOUT(
+    LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5), LGUI(KC_6), LGUI(KC_7), LGUI(KC_8), LGUI(KC_9), LGUI(KC_0),
+    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_UP,      _______,
+    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_LEFT,    KC_DOWN,    KC_RGHT,
+    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_BRMD,    KC_UP,      KC_BRMU
+),
+
+/* MAC (Key Position Swap for Mac Compatibility)
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+ * │   │   │   │   │   │   │   │   │   │   │
+ * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+ * │   │   │   │   │   │   │   │   │   │   │
+ * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+ * │   │   │   │   │   │   │   │   │   │   │
+ * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
+ * │<>|│ " │   │   │   │   │   │   │   │   │
+ * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+ */
+[_MAC] = LAYOUT(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    KC_NONUS_BACKSLASH, KC_GRV, _______, _______, _______, _______, _______, _______, _______, _______
 )
 };
+
+#ifdef POINTING_DEVICE_DRIVER
+// DPI setting for trackpad
+void keyboard_post_init_user(void) {
+    pointing_device_set_cpi(500);
+}
+#endif
+
+// Layer state handling for ADJUST layer (activated when LOWER + RAISE are both active)
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _LOWER, _SPACE, _ADJUST);
+}
+
+// Auto Moving Mouse to prevent screen lock
+bool moving_mouse = false;
+uint16_t last_mouse_move = 0;
+void toggle_infinite_mouse_movement(void) {
+    moving_mouse = !moving_mouse;
+}
+void matrix_scan_user(void) {
+    if (moving_mouse && timer_elapsed(last_mouse_move) > 5000) {
+        tap_code(KC_MS_L);  // Move 1px left
+        wait_ms(10);
+        tap_code(KC_MS_R);  // Move 1px right
+        last_mouse_move = timer_read();  // Reset timer
+    }
+}
+
+// Fix Windows and MacOS inconsistency between lower than and greater than keys on Turkish layout
+// Grave and Non US Backslash keys are swapped on Turkish layout on Windows and MacOS
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // Handle MO_NUMPAD custom keycode
+  if (keycode == MO_NUMPAD) {
+    if (record->event.pressed) {
+      layer_on(_NUMPAD);
+    } else {
+      layer_off(_NUMPAD);
+    }
+    return false;
+  }
+  
+  if(IS_LAYER_ON(_MAC)) {
+    switch (keycode) {
+      case KC_NONUS_BACKSLASH:
+        if (record->event.pressed) {
+            register_code(KC_GRV);
+        } else {
+            unregister_code(KC_GRV);
+        }
+        return false; // Skip all further processing of this key
+      case KC_GRV:
+        if (record->event.pressed) {
+            register_code(KC_NONUS_BACKSLASH);
+        } else {
+            unregister_code(KC_NONUS_BACKSLASH);
+        }
+        return false; // Skip all further processing of this key
+      case MOUSE_INFINITE_TOGGLE:
+        if (record->event.pressed) {
+            toggle_infinite_mouse_movement();
+        }
+        return false;  // Skip further processing for this key
+      default:
+        return true; // Process all other keycodes normally
+    }
+  } else {
+    // When not on MAC layer, still handle MOUSE_INFINITE_TOGGLE
+    if (keycode == MOUSE_INFINITE_TOGGLE && record->event.pressed) {
+        toggle_infinite_mouse_movement();
+        return false;
+    }
+  }
+  return true; // Process all other keycodes normally
+}
+
+// OS Detection - Automatically enable MAC layer on macOS
+bool process_detected_host_os_user(os_variant_t detected_os) {
+    switch (detected_os) {
+        case OS_MACOS:
+        case OS_IOS:
+            if (!IS_LAYER_ON(_MAC)) {
+                layer_on(_MAC);
+            }
+            break;
+        case OS_WINDOWS:
+        case OS_LINUX:
+            if (IS_LAYER_ON(_MAC)) {
+                layer_off(_MAC);
+            }
+            break;
+        case OS_UNSURE:
+            if (IS_LAYER_ON(_MAC)) {
+                layer_off(_MAC);
+            }
+            break;
+    }
+    return true;
+}
 
--- keyboards/cost_crafter40/keymaps/ardakilic/rules.mk
+++ keyboards/cost_crafter40/keymaps/ardakilic/rules.mk
@@ -3,6 +3,12 @@ COMBO_ENABLE = yes
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = azoteq_iqs5xx
 
+# Enable OS Detection for automatic Mac/Windows layer switching
+OS_DETECTION_ENABLE = yes
+
+# Enable mousekey support for combos
+MOUSEKEY_ENABLE = yes
+
 # Fix for unused function warning in I2C driver on RP2040
 # error: 'i2c_lld_serve_interrupt' defined but not used [-Werror=unused-function]
 # EXTRAFLAGS += -Wno-unused-function
